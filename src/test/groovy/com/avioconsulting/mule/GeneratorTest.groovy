1f2db0a895906d2561dfc163130196881ea44689
Brady Wied <brady@wied.us>
2017-05-27 11:07:15 -0600

    setup for more tests

--- src/test/groovy/com/avioconsulting/mule/GeneratorTest.groovy
+++ src/test/groovy/com/avioconsulting/mule/GeneratorTest.groovy
@@ -1,30 +1,37 @@
 package com.avioconsulting.mule
 
 import org.apache.commons.io.FileUtils
+import org.junit.Before
 import org.junit.Test
 
-import static org.hamcrest.Matchers.equalTo
-import static org.hamcrest.Matchers.is
+import static org.hamcrest.Matchers.*
 import static org.junit.Assert.assertThat
 
 @SuppressWarnings("GroovyAssignabilityCheck")
 class GeneratorTest implements FileUtil {
-    @Test
-    void generates_Flow() {
-        // arrange
-        def tempDir = join new File('build'), 'tmp', 'test'
+    private File tempDir, appDir
+
+    @Before
+    void setup() {
+        tempDir = join new File('build'), 'tmp', 'test'
         if (tempDir.exists()) {
             tempDir.deleteDir()
         }
         tempDir.mkdirs()
         def mainDir = join tempDir, 'src', 'main'
         mainDir.mkdirs()
-        def appDir = join mainDir, 'app'
+        appDir = join mainDir, 'app'
         appDir.mkdirs()
         def apiDir = join mainDir, 'api'
         apiDir.mkdirs()
         def sourceFile = join new File('src'), 'test', 'resources', 'api-stuff-v1.raml'
         FileUtils.copyFileToDirectory(sourceFile, apiDir)
+    }
+
+    @Test
+    void generates_Flow() {
+        // arrange
+
 
         // act
         Generator.generate(tempDir, 'api-stuff-v1.raml')
