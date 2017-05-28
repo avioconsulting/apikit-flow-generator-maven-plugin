82d1821324c9bfa7062ff47035200b4b561697b4
Brady Wied <brady@wied.us>
2017-05-28 13:24:32 -0600

    Move this functionality to archetype

--- src/main/groovy/com/avioconsulting/mule/Generator.groovy
+++ src/main/groovy/com/avioconsulting/mule/Generator.groovy
@@ -1,14 +1,8 @@
 package com.avioconsulting.mule
 
 import groovy.xml.Namespace
-import org.apache.commons.io.IOUtils
 import org.codehaus.plexus.util.FileUtils
 import org.mule.tools.apikit.ScaffolderAPI
-import sun.security.tools.keytool.CertAndKeyGen
-import sun.security.x509.*
-
-import java.security.KeyStore
-import java.security.cert.Certificate
 
 class Generator implements FileUtil {
     private static final xmlParser = new XmlParser(false, true)
@@ -43,12 +37,9 @@ class Generator implements FileUtil {
         }
         def flowPath = new File(appDirectory, flowFileName)
         assert flowPath.exists()
-        updateMuleDeployProperties(appDirectory)
         alterGeneratedFlow(flowPath,
                            apiName,
                            apiVersion)
-        setupGlobalConfig(appDirectory)
-        generateKeyStore(mainDir)
     }
 
     private static void adjustRamlBaseUri(File ramlFile, String mavenProjectName) {
@@ -57,59 +48,6 @@ class Generator implements FileUtil {
         def fixedRaml = ramlText.replaceAll(/baseUri: .*/,
                                             "baseUri: ${baseUri}")
         ramlFile.write fixedRaml
-    }
-
-    private static void generateKeyStore(File mainDir) {
-        def keystoreDir = join(mainDir, 'resources', 'keystores')
-        keystoreDir.mkdirs()
-        def keystoreFile = join(keystoreDir, 'listener_keystore.jks')
-        if (keystoreFile.exists()) {
-            return
-        }
-        def keystore = KeyStore.getInstance('jks')
-        def keystorePassword = 'developmentKeystorePassword'.toCharArray()
-        keystore.load(null, keystorePassword)
-        def keyGen = new CertAndKeyGen('RSA', 'SHA1WithRSA', null)
-        keyGen.generate(1024)
-        CertificateExtensions certificateExtensions = getCertExtensions()
-        def cert = keyGen.getSelfCertificate(new X500Name('CN=ROOT'),
-                                             new Date(),
-                                             // 20 years
-                                             (long) 20 * 365 * 24 * 3600,
-                                             certificateExtensions)
-        Certificate[] chain = [cert]
-        keystore.setKeyEntry('selfsigned',
-                             keyGen.privateKey,
-                             keystorePassword,
-                             chain)
-        def stream = keystoreFile.newOutputStream()
-        keystore.store(stream, keystorePassword)
-        stream.close()
-    }
-
-    // Chrome no longer accepts certs without a subject name
-    private static CertificateExtensions getCertExtensions() {
-        def certificateExtensions = new CertificateExtensions()
-        def names = new GeneralNames()
-        names.add(new GeneralName(new DNSName('localhost')))
-        def extension = new SubjectAlternativeNameExtension(names)
-        certificateExtensions.set(SubjectAlternativeNameExtension.NAME,
-                                  extension)
-        certificateExtensions
-    }
-
-    private static void setupGlobalConfig(File appDirectory) {
-        def globalXmlPath = join(appDirectory, 'global.xml')
-        if (globalXmlPath.exists()) {
-            return
-        }
-        def input = this.getResourceAsStream('/global_template.xml')
-        assert input
-        def stream = globalXmlPath.newOutputStream()
-        IOUtils.copy(input, stream)
-        stream.close()
-        def xmlNode = xmlParser.parse(globalXmlPath)
-        new XmlNodePrinter(new IndentPrinter(new FileWriter(globalXmlPath))).print xmlNode
     }
 
     private static void alterGeneratedFlow(File flowPath,
@@ -145,20 +83,5 @@ class Generator implements FileUtil {
         httpListenerConfigs.each { config ->
             flowNode.remove(config)
         }
-    }
-
-    private static void updateMuleDeployProperties(File appDirectory) {
-        def muleDeployProperties = new Properties()
-        def propsPath = join(appDirectory, 'mule-deploy.properties')
-        muleDeployProperties.load(propsPath.newInputStream())
-        String existingResourceString = muleDeployProperties['config.resources']
-        def existingResources = existingResourceString.split(',').collect { String s -> s.trim() }
-        if (existingResources.contains('global.xml')) {
-            return
-        }
-        def newResources = ['global.xml'] + existingResources
-        muleDeployProperties['config.resources'] = newResources.join(',')
-        muleDeployProperties.store(propsPath.newOutputStream(),
-                                   'Generated file')
     }
 }
