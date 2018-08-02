0f2c39c0f98758d7130804f0949e02f1f2b44e9b
Brady Wied <brady@bswtechconsulting.com>
2018-08-01 22:22:41 -0600

    Add mojo implementation

--- src/main/groovy/com/avioconsulting/mule/SoapGenerateMojo.groovy
+++ src/main/groovy/com/avioconsulting/mule/SoapGenerateMojo.groovy
@@ -3,8 +3,10 @@ package com.avioconsulting.mule
 import org.apache.maven.plugin.AbstractMojo
 import org.apache.maven.plugin.MojoExecutionException
 import org.apache.maven.plugin.MojoFailureException
+import org.apache.maven.plugins.annotations.Component
 import org.apache.maven.plugins.annotations.Mojo
 import org.apache.maven.plugins.annotations.Parameter
+import org.apache.maven.project.MavenProject
 
 @Mojo(name = 'generateFlowSoap')
 class SoapGenerateMojo extends AbstractMojo {
@@ -23,8 +25,16 @@ class SoapGenerateMojo extends AbstractMojo {
     @Parameter(property = 'http.listener.config.name', required = true)
     private String httpListenerConfigName
 
+    @Component
+    protected MavenProject mavenProject
+
     @Override
     void execute() throws MojoExecutionException, MojoFailureException {
-
+        SoapGenerator.generate(mavenProject.basedir,
+                               wsdlPath,
+                               apiVersion,
+                               wsdlService,
+                               wsdlPort,
+                               httpListenerConfigName)
     }
 }
