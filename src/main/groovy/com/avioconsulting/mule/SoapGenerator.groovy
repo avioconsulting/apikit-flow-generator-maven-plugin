a5ae0cfd70430bbaf902ca4642328621ff98427d
Brady Wied <brady@bswtechconsulting.com>
2019-01-18 10:26:53 -0700

    First cut of doing it the simple way

--- src/main/groovy/com/avioconsulting/mule/SoapGenerator.groovy
+++ src/main/groovy/com/avioconsulting/mule/SoapGenerator.groovy
@@ -1,18 +1,9 @@
 package com.avioconsulting.mule
 
+import com.avioconsulting.mule.resources.SoapResources
 import org.apache.commons.io.FilenameUtils
-import org.jdom2.output.Format
-import org.jdom2.output.XMLOutputter
-import org.mule.runtime.app.declaration.api.ArtifactDeclaration
-import org.mule.runtime.app.declaration.api.fluent.ElementDeclarer
-import org.mule.soapkit.scaffolder.Scaffolder
-import org.mule.tooling.soapkit.scaffolder.ArtifactDeclarationUtils
-
-import java.util.regex.Pattern
 
 class SoapGenerator implements FileUtil {
-    private static final Pattern listenerPattern = Pattern.compile(/<http:listener path="(.*?)"/)
-
     static void generate(File baseDirectory,
                          File wsdlPath,
                          String apiName,
@@ -30,7 +21,7 @@ class SoapGenerator implements FileUtil {
                            'resources',
                            'api')
         def appDir = join(mainDir,
-                          'app')
+                          'mule')
         def generatedFilenameOnly = "${FilenameUtils.getBaseName(wsdlPath.name)}_${version}.xml"
         def outputFile = new File(appDir,
                                   generatedFilenameOnly)
@@ -38,53 +29,29 @@ class SoapGenerator implements FileUtil {
             // plugin currently duplicates existing files, so don't try and support this
             throw new Exception('You can only use this plugin to do the initial generation of flows from WSDL. Use Studio to perform updates!')
         }
-        try {
-            def emptyDomain = ArtifactDeclarationUtils.emptyDeclaration()
-            def result = Scaffolder.instance.scaffold(wsdlPathStr,
-                                                      wsdlPathStr,
-                                                      service,
-                                                      port,
-                                                      emptyDomain)
-            def project = new MuleAppProject()
-            ArtifactDeclarationUtils.serializeDeclaration(project,
-                                                          result)
-            def outputter = new XMLOutputter()
-            outputter.format = Format.getPrettyFormat()
-            outputter.output(result,
-                             new FileWriter(outputFile))
-            // don't want an absolute path in here, need it relative to src/main/wsdl
-            // project will ensure wsdl directory is at root of ZIP
-            def relativeWsdl = wsdlDir.toPath().relativize(wsdlPath.toPath()).toString()
-            def fileXml = outputFile.text
-            def newListenerPrefix = insertApiNameInListenerPath ? "/${apiName}/${version}" :
-                    "/${version}"
-            fileXml = fileXml.replaceAll(listenerPattern,
-                                         "<http:listener path=\"${newListenerPrefix}\$1\"")
-                    .replace('<apikit-soap:config',
-                             '<apikit-soap:config inboundValidationMessage="${validate.soap.requests}"')
-                    .replace(wsdlPath.absolutePath,
-                             relativeWsdl)
-            if (insertXmlBeforeRouter) {
-                fileXml = fileXml.replace('<apikit-soap:router',
-                                          "${insertXmlBeforeRouter}\r\n    <apikit-soap:router")
-            }
-            outputFile.text = fileXml
-            def muleDeployProps = new Properties()
-            def muleDeployPropsFile = new File(appDir,
-                                               'mule-deploy.properties')
-            muleDeployProps.load(new FileInputStream(muleDeployPropsFile))
-            def configResources = muleDeployProps.getProperty('config.resources').split(',').collect { p ->
-                p.trim()
-            }
-            configResources << generatedFilenameOnly.toString()
-            muleDeployProps.setProperty('config.resources',
-                                        configResources.join(','))
-            muleDeployProps.store(new FileOutputStream(muleDeployPropsFile),
-                                  'Updated by apikit flow generator plugin')
-            cleanProps(muleDeployPropsFile)
+        outputFile.text = SoapResources.HEADER +
+                '\n' +
+                SoapResources.MAIN_FLOW +
+                '\n' +
+                SoapResources.OPERATION_TEMPLATE +
+                '\n' +
+                SoapResources.FOOTER
+        // don't want an absolute path in here, need it relative to src/main/wsdl
+        // project will ensure wsdl directory is at root of ZIP
+        def relativeWsdl = wsdlDir.toPath().relativize(wsdlPath.toPath()).toString()
+        def fileXml = outputFile.text
+        def newListenerPrefix = insertApiNameInListenerPath ? "/${apiName}/${version}" :
+                "/${version}"
+//            fileXml = fileXml.replaceAll(listenerPattern,
+//                                         "<http:listener path=\"${newListenerPrefix}\$1\"")
+//                    .replace('<apikit-soap:config',
+//                             '<apikit-soap:config inboundValidationMessage="${validate.soap.requests}"')
+//                    .replace(wsdlPath.absolutePath,
+//                             relativeWsdl)
+        if (insertXmlBeforeRouter) {
+            fileXml = fileXml.replace('<apikit-soap:router',
+                                      "${insertXmlBeforeRouter}\r\n    <apikit-soap:router")
         }
-        finally {
-
-        }
+        outputFile.text = fileXml
     }
 }
---
+++ src/main/groovy/com/avioconsulting/mule/resources/SoapResources.groovy
@@ -0,0 +1,53 @@
+package com.avioconsulting.mule.resources
+
+// would normally be in src/main/resources but Maven plugins do not resolve those resources
+class SoapResources {
+    static final String HEADER = '<?xml version="1.0" encoding="UTF-8" standalone="no"?>\n' +
+            '<mule xmlns="http://www.mulesoft.org/schema/mule/core" xmlns:apikit-soap="http://www.mulesoft.org/schema/mule/apikit-soap" xmlns:doc="http://www.mulesoft.org/schema/mule/documentation" xmlns:ee="http://www.mulesoft.org/schema/mule/ee/core" xmlns:http="http://www.mulesoft.org/schema/mule/http" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.mulesoft.org/schema/mule/core http://www.mulesoft.org/schema/mule/core/current/mule.xsd http://www.mulesoft.org/schema/mule/http http://www.mulesoft.org/schema/mule/http/current/mule-http.xsd http://www.mulesoft.org/schema/mule/apikit-soap http://www.mulesoft.org/schema/mule/apikit-soap/current/mule-apikit-soap.xsd http://www.mulesoft.org/schema/mule/ee/core http://www.mulesoft.org/schema/mule/ee/core/current/mule-ee.xsd">'
+
+    static final String MAIN_FLOW = '<flow name="api-main">\n' +
+            '        <http:listener config-ref="THE_LISTENER_CONFIG" path="THE_LISTENER_PATH">\n' +
+            '            <http:response>\n' +
+            '                <http:body>#[payload]</http:body>\n' +
+            '                <http:headers>#[attributes.protocolHeaders default {}]</http:headers>\n' +
+            '            </http:response>\n' +
+            '            <http:error-response>\n' +
+            '                <http:body>#[payload]</http:body>\n' +
+            '                <http:headers>#[attributes.protocolHeaders default {}]</http:headers>\n' +
+            '            </http:error-response>\n' +
+            '        </http:listener>\n' +
+            '        <apikit-soap:router config-ref="soapkit-config">\n' +
+            '            <apikit-soap:message>#[payload]</apikit-soap:message>\n' +
+            '            <apikit-soap:attributes>#[\n' +
+            '              %dw 2.0\n' +
+            '              output application/java\n' +
+            '              ---\n' +
+            '              {\n' +
+            '                  headers: attributes.headers,\n' +
+            '                  method: attributes.method,\n' +
+            '                  queryString: attributes.queryString\n' +
+            '            }]</apikit-soap:attributes>\n' +
+            '        </apikit-soap:router>\n' +
+            '    </flow>'
+
+    static final String OPERATION_TEMPLATE = '<flow name="OPERATION_NAME:\\soapkit-config">\n' +
+            '        <ee:transform>\n' +
+            '            <ee:message>\n' +
+            '                <ee:set-payload><![CDATA[%dw 2.0\n' +
+            'output application/java\n' +
+            'ns soap http://schemas.xmlsoap.org/soap/envelope\n' +
+            '---\n' +
+            '{\n' +
+            '    body: {\n' +
+            '        soap#Fault: {\n' +
+            '            faultcode: "soap:Server",\n' +
+            '            faultstring: "Operation [operation1:\\soapkit-config] not implemented"\n' +
+            '        }\n' +
+            '    } write "application/xml"\n' +
+            '}]]></ee:set-payload>\n' +
+            '            </ee:message>\n' +
+            '        </ee:transform>\n' +
+            '    </flow>'
+
+    static final String FOOTER = '</mule>'
+}
