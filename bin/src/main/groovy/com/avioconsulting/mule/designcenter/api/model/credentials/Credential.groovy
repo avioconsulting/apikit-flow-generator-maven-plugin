f6dfb2769c13edf39c03fc433dfefaf5060e9ea2
Juan Cruz Basso <jcbasso@avioconsulting.com>
2023-10-15 15:23:19 -0300

    feat: removing bin folder, and adding it into gitignore file

--- bin/src/main/groovy/com/avioconsulting/mule/designcenter/api/model/credentials/Credential.groovy
+++
@@ -1,9 +0,0 @@
-package com.avioconsulting.mule.designcenter.api.models.credentials;
-
-/**
- * Abstract Base class for defining anypoint platform access credentials
- */
-abstract class Credential {
-
-    abstract String getPrincipal()
-}
