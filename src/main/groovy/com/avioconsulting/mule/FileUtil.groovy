d764b1f41f3d2478554599377945d274dbe12646
Adam DesJardin <adesjardin@gmail.com>
2024-09-17 09:50:42 -0500

    fix: Changed isBinary to isText and updated test cases

--- src/main/groovy/com/avioconsulting/mule/FileUtil.groovy
+++ src/main/groovy/com/avioconsulting/mule/FileUtil.groovy
@@ -10,22 +10,19 @@ trait FileUtil {
     }
 
     /* this is used to determine if the file is binary, ie. don't modify it */
-    static boolean isBinary(Path path) {
+    static boolean isText(Path path) {
         try {
             byte[] bytes = Files.readAllBytes(path)
-            return !isText(bytes)
+            for (byte b : bytes) {
+                // Non-printable bytes (excluding common control characters) indicate binary content
+                if (b < 32 && b != 9 && b != 10 && b != 13) {
+                    return false
+                }
+            }
+            return true
         } catch (IOException e) {
             e.printStackTrace()
             return true
         }
-    }
-    private static boolean isText(byte[] bytes) {
-        for (byte b : bytes) {
-            // Non-printable bytes (excluding common control characters) indicate binary content
-            if (b < 32 && b != 9 && b != 10 && b != 13) {
-                return false
-            }
-        }
-        return true
     }
 }
